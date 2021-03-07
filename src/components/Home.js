3a71ca2ea305ad0dc3326bda5e0fa46d6f68bffe
thomasjk0831 <thomaskim08310@gmail.com>
2021-03-07 18:30:44 -0500

    added button link in Home comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+  let history = useHistory();
+
+  function clickLink(e) {
+    e.preventDefault();
+    history.push("/items");
+  }
+
   return (
     <div className="home-wrapper">
       <img
@@ -8,7 +16,9 @@ function Home() {
         src="http://www.sanctuaryrecordsgroup.com/wp-content/uploads/2017/05/banner-eCommerce-Solutions.jpg"
         alt=""
       />
-      <button className="md-button shop-button">Shop now!</button>
+      <button className="md-button shop-button" onClick={clickLink}>
+        Shop now!
+      </button>
     </div>
   );
 }
