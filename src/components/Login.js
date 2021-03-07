f93f5e9d0eb084524fd939c73846496e675f80a7
thomasjk0831 <thomaskim08310@gmail.com>
2021-03-07 10:23:17 -0500

    setup Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
-  return <div>Login</div>;
+function Login(props) {
+  const [choice, setChoice] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.setUser(choice);
+  }
+
+  function handleChange(e) {
+    setChoice(parseInt(e.target.value));
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Login
+          <select onChange={handleChange}>
+            <option value="1">John</option>
+            <option value="2">Jane</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Login;
