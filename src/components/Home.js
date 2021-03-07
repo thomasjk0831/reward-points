ab6bdbab2a347a6e95e9aee6f30a5d2592bb7385
thomasjk0831 <thomaskim08310@gmail.com>
2021-03-07 11:03:24 -0500

    setup home comp and points

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home-wrapper">
+      <img
+        className="home-image"
+        src="https://www.uncommongoods.com/images/category/fun-fullwidth.jpg"
+        alt=""
+      />
+      <button className="md-button shop-button">Shop now!</button>
+    </div>
+  );
 }
 
 export default Home;
--- src/components/Points.js
+++ src/components/Points.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Points() {
-  return <div>Points</div>;
+function Points(props) {
+  const [ptsCurrentMonth, setPtsCurrentMonth] = useState(null);
+  const [pts2ndToLastMonth, setPts2ndToLastMonth] = useState(null);
+  const [ptsLastMonth, setPtsLastMonth] = useState(null);
+
+  useEffect(() => {
+    function getPoints() {
+      //gets the current month as integer
+      const currentMonth = new Date().getMonth() + 1;
+
+      let tempPtsCurrent = 0;
+      let tempPts2ndToLast = 0;
+      let tempPtsLast = 0;
+
+      //loop through the purchases array and update points per month per user
+      props.purchases.map((item) => {
+        if (item.user !== props.user) return;
+        else {
+          if (currentMonth === item.month) {
+            tempPtsCurrent += calculatePoints(item.amount);
+          } else if (currentMonth - 1 === item.month) {
+            tempPts2ndToLast += calculatePoints(item.amount);
+          } else if (currentMonth - 2 === item.month) {
+            tempPtsLast += calculatePoints(item.amount);
+          } else if (currentMonth - 1 < 1 && currentMonth + 11 === item.month) {
+            tempPts2ndToLast += calculatePoints(item.amount);
+          } else if (currentMonth - 2 < 1 && currentMonth + 10 === item.month) {
+            tempPtsLast += calculatePoints(item.amount);
+          }
+          return;
+        }
+      });
+
+      setPtsCurrentMonth(tempPtsCurrent);
+      setPts2ndToLastMonth(tempPts2ndToLast);
+      setPtsLastMonth(tempPtsLast);
+    }
+
+    getPoints();
+  }, []);
+
+  function calculatePoints(amount) {
+    let points = 0;
+    amount = Math.floor(amount);
+
+    if (amount > 100) {
+      points += (amount - 100) * 2;
+      amount = 100;
+    }
+    if (amount > 50 && amount <= 100) {
+      points += amount - 50;
+      amount = 50;
+    }
+    if (amount <= 50) {
+      return points;
+    }
+  }
+
+  return (
+    <div>
+      <p>{ptsCurrentMonth}</p>
+      <p>{pts2ndToLastMonth}</p>
+      <p>{ptsLastMonth}</p>
+      <p>{ptsLastMonth + pts2ndToLastMonth + ptsCurrentMonth}</p>
+    </div>
+  );
 }
 
 export default Points;
