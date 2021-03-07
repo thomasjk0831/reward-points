6c5e25b48969413011ffba84920e0e1f46105609
thomasjk0831 <thomaskim08310@gmail.com>
2021-03-07 10:20:02 -0500

    setup items component

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-function Items() {
-  return <div>Items</div>;
+function Items(props) {
+  return (
+    <div className="items-list-wrapper">
+      {props.items.map((item) => (
+        <div className="item-card" key={item.id}>
+          <img
+            className="item-list-image"
+            src={item.imageUrl}
+            alt={item.name}
+          />
+          <p>{item.name}</p>
+          <p>${item.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Items;
